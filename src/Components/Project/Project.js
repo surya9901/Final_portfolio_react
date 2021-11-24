ed84747de2acb08e3ed2d837890fe739f8c1d646
surya9901 <surya9901@gmail.com>
2021-11-24 12:54:22 +0530

    updated projects

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -27,21 +27,6 @@ function Project() {
         "github_link": "https://github.com/surya9901/URL-Shortner",
         "site_link": "https://gshort-urlshortner.herokuapp.com/",
     }, {
-        "title": "Sb2 Admin",
-        "description": "This is a React Admin web app used to display the CRUD operations, built using JavaScript, React, Axios, Usecontext Hook, React router dom and Bootstrap.",
-        "github_link": "https://github.com/surya9901/sb2admin",
-        "site_link": "https://sb2admin.netlify.app/product",
-    }, {
-        "title": "UseState in React",
-        "description": "This is a React web app used to display how UseState works, it was built using JavaScript, React and Bootstrap.",
-        "github_link": "https://github.com/surya9901/use-state-react",
-        "site_link": "https://619bcdf815ce2745f8b109d5--frosty-lumiere-2be181.netlify.app/",
-    }, {
-        "title": "E-commercer",
-        "description": "This is a simple e-commerce web app template used to display how e-commerce works, it was built using JavaScript, React and Bootstrap.",
-        "github_link": "https://github.com/surya9901/ecommerce-react",
-        "site_link": "https://peaceful-panini-b18b92.netlify.app/",
-    }, {
         "title": "Nationalize",
         "description": "This is a web app which uses Api to display the Nationality & Probability by giving your name as the input, it was built using JavaScript, DOM and Bootstrap.",
         "github_link": "https://github.com/surya9901/Nationalize",
@@ -51,29 +36,19 @@ function Project() {
         "description": "This is a web app which uses Api to display the Animie details by giving your name of the animie as the input, it was built using JavaScript, DOM and Bootstrap.",
         "github_link": "https://github.com/surya9901/Dom_Manipulation_Annimie_API",
         "site_link": "https://animationapi.netlify.app/",
-    }, {
-        "title": "Cards",
-        "description": "This is a simple web app which used to display cards with effects and see how props work, it was built using JavaScript, React and Bootstrap.",
-        "github_link": "https://github.com/surya9901/Cards-react",
-        "site_link": "https://surya9901.github.io/Cards-react/",
-    }, {
-        "title": "Pagination",
-        "description": "This is a simple web app which used to display on how the pagination works, it was built using JavaScript, DOM and Bootstrap.",
-        "github_link": "https://github.com/surya9901/Pagination",
-        "site_link": "https://reverent-edison-e2d95f.netlify.app/",
     }]
 
 
     return (
         <div className="container mb-3">
-            <div className="cv_preview mt-3" data-aos="flip-left">
+            <div className="cv_preview mt-1 mb-3" data-aos="flip-left">
                 <a className="btn" href={env.resume} target="_blank" rel="noreferrer noopener" >
                     Resume{" "}
                     <i className="fas fa-cloud-download-alt"></i>
                 </a>
             </div>
 
-            <div className="row row-cols-1 row-cols-md-3 g-4 mt-1" data-aos="fade-in">
+            <div className="row row-cols-1 row-cols-md-3 g-4" data-aos="fade-in">
                 {
                     project.map(obj => {
                         return (
