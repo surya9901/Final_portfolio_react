b24a0e896d5ef767dda9f9f73c22f1bf0165baa7
surya9901 <surya9901@gmail.com>
2021-11-27 10:43:59 +0530

    changed resume url

--- src/Components/settings.js
+++ src/Components/settings.js
@@ -1,7 +1,7 @@
 let getSettings = () => {
     return {
         api: "https://suryas-port-contact-form.herokuapp.com",
-        resume: "https://drive.google.com/file/d/1EmIrNgkyl5xKAkha4JwwdQP3Zq_2tfkE/view?usp=sharing"
+        resume: "https://drive.google.com/file/d/1sl7DI9nfVuH3t2Q4lZcU0ZLsbYxW9kJx/view?usp=sharing"
         // api: "http://localhost:5000"
     }
 }
