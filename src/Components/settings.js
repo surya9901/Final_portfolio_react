e3c54e46bcaf3f89e153edd0164f3ba976747866
surya9901 <surya9901@gmail.com>
2021-12-10 00:40:16 +0530

    resume url updated

--- src/Components/settings.js
+++ src/Components/settings.js
@@ -1,7 +1,7 @@
 let getSettings = () => {
     return {
         api: "https://suryas-port-contact-form.herokuapp.com",
-        resume: "https://drive.google.com/file/d/1cA3XXe5BE5sdAni-vlGF6yHyCLaCCAkM/view?usp=sharing"
+        resume: "https://drive.google.com/file/d/1TJHl_6qZE57JlHbdyYluNd-K0dsyjgqv/view?usp=sharing"
         // api: "http://localhost:5000"
     }
 }
